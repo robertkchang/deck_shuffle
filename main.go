e1cb15f60966bab2ef16456e18127da1ada761c8
Robert Chang <robertkchang@yahoo.com>
2017-07-16 14:07:25 -0700

    update spaces

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
+  "fmt"
 
-	deck "github.com/robertkchang/deck_shuffle/deck"
-	shuffle "github.com/robertkchang/deck_shuffle/shuffle"
+  deck "github.com/robertkchang/deck_shuffle/deck"
+  shuffle "github.com/robertkchang/deck_shuffle/shuffle"
 )
 
 func main() {
-	fmt.Println("=======================")
+  fmt.Println("=======================")
 
-	// init
-	original := deck.New()
-	fmt.Println("[ORIGINAL] ", original)
+  // init
+  original := deck.New()
+  fmt.Println("[ORIGINAL] ", original)
 
-	fmt.Println("=======================")
+  fmt.Println("=======================")
 
-	// shuffle
-	shuffled := shuffle.Shuffle(original)
-	fmt.Println("[SHUFFLED] ", shuffled)
+  // shuffle
+  shuffled := shuffle.Shuffle(original)
+  fmt.Println("[SHUFFLED] ", shuffled)
 }
--- shuffle/shuffle.go
+++ shuffle/shuffle.go
@@ -1,10 +1,10 @@
 package shuffle
 
 import (
-	"math/rand"
-	"time"
+  "math/rand"
+  "time"
 
-	deck "github.com/robertkchang/deck_shuffle/deck"
+  deck "github.com/robertkchang/deck_shuffle/deck"
 )
 
 // Shuffle the deck
@@ -12,33 +12,33 @@ import (
 // thus, when the slice is updated, the array values are updated
 // https://stackoverflow.com/questions/39993688/are-golang-slices-pass-by-value
 func Shuffle(arr []deck.Card) []deck.Card {
-	// get random generator with new seed
-	randomGen := getRandomGen()
+  // get random generator with new seed
+  randomGen := getRandomGen()
 
-	// using Durstenfeld variation of the Fisher-Yates Shuffle algorithm
-	// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-	for idx := len(arr) - 1; idx > 0; idx-- {
-		// random int 0 <= rndNum < idx
-		rndNum := randomGen.Intn(idx)
+  // using Durstenfeld variation of the Fisher-Yates Shuffle algorithm
+  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+  for idx := len(arr) - 1; idx > 0; idx-- {
+    // random int 0 <= rndNum < idx
+    rndNum := randomGen.Intn(idx)
 
-		// swap idx and rndNum
-		arr = swap(arr, idx, rndNum)
-	}
-	return arr
+    // swap idx and rndNum
+    arr = swap(arr, idx, rndNum)
+  }
+  return arr
 }
 
 // get random generator
 func getRandomGen() *rand.Rand {
-	// set random seed to current time
-	seed := time.Now().UnixNano()
-	seedSource := rand.NewSource(seed)
-	return rand.New(seedSource)
+  // set random seed to current time
+  seed := time.Now().UnixNano()
+  seedSource := rand.NewSource(seed)
+  return rand.New(seedSource)
 }
 
 // swap two array elements
 func swap(arr []deck.Card, firstIdx int, secondIdx int) []deck.Card {
-	tmp := arr[firstIdx]
-	arr[firstIdx] = arr[secondIdx]
-	arr[secondIdx] = tmp
-	return arr
+  tmp := arr[firstIdx]
+  arr[firstIdx] = arr[secondIdx]
+  arr[secondIdx] = tmp
+  return arr
 }
