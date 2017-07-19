8cb213ab146eab4d36797908f99557e98a17703b
Chang, Robert (Contractor) <Robert.Chang@nordstrom.com>
2017-07-19 12:21:27 -0700

    refactor TestShuffle tests

--- deck/deck.go
+++ deck/deck.go
@@ -1,24 +1,24 @@
 package deck
 
 import (
-  "errors"
-  "math/rand"
-  "time"
+	"errors"
+	"math/rand"
+	"time"
 )
 
 // New deck of cards
 func New() []Card {
-  deck := make([]Card, 52)
+	deck := make([]Card, 52)
 
-  var cardCnt = 0
-  for _, suite := range Suites {
-    for _, value := range Values {
-      deck[cardCnt] = Card{Suite: suite, Value: value}
-      cardCnt++
-    }
-  }
+	var cardCnt = 0
+	for _, suite := range Suites {
+		for _, value := range Values {
+			deck[cardCnt] = Card{Suite: suite, Value: value}
+			cardCnt++
+		}
+	}
 
-  return deck
+	return deck
 }
 
 // Shuffle the deck
@@ -26,22 +26,22 @@ func New() []Card {
 // thus, when the slice is updated, the array values are updated
 // https://stackoverflow.com/questions/39993688/are-golang-slices-pass-by-value
 func Shuffle(arr []Card) ([]Card, error) {
-  // check if deck is nil or empty
-  if arr == nil || len(arr) == 0 {
-    return nil, errors.New("deck.Card is missing or empty")
-  }
+	// check if deck is nil or empty
+	if arr == nil || len(arr) == 0 {
+		return nil, errors.New("Card is missing or empty")
+	}
 
-  // get random generator with new seed
-  randomGen := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// get random generator with new seed
+	randomGen := rand.New(rand.NewSource(time.Now().UnixNano()))
 
-  // using Durstenfeld variation of the Fisher-Yates Shuffle algorithm
-  // https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-  for idx := len(arr) - 1; idx > 0; idx-- {
-    // random int 0 <= rndNum < idx
-    rndNum := randomGen.Intn(idx)
+	// using Durstenfeld variation of the Fisher-Yates Shuffle algorithm
+	// https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+	for idx := len(arr) - 1; idx > 0; idx-- {
+		// random int 0 <= rndNum < idx
+		rndNum := randomGen.Intn(idx)
 
-    // swap idx and rndNum
-    arr[idx], arr[rndNum] = arr[rndNum], arr[idx]
-  }
-  return arr, nil
+		// swap idx and rndNum
+		arr[idx], arr[rndNum] = arr[rndNum], arr[idx]
+	}
+	return arr, nil
 }
--- deck/deck_test.go
+++ deck/deck_test.go
@@ -1,62 +1,80 @@
 package deck
 
 import (
-  "github.com/stretchr/testify/assert"
-  "testing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestNew(t *testing.T) {
-  newDeck := New()
-  assert.Equal(t, len(newDeck), 52, "New deck should contain 52 cards")
+	newDeck := New()
+	assert.Equal(t, len(newDeck), 52, "New deck should contain 52 cards")
 }
 
-func TestShuffle(t *testing.T) {
-  // init new deck
-  newDeck := New()
+func TestShuffleSameNumOfCards(t *testing.T) {
+	// init new deck and shuffle
+	originalDeck, shuffledDeck := createNewDeckAndShuffle()
 
-  // make copy of new deck since shuffle will change newDeck
-  var originalDeck = make([]Card, 52)
-  copy(originalDeck, newDeck)
+	// validate same number of cards returned
+	assert.Equal(t, len(originalDeck), len(shuffledDeck), "Shuffled deck does not contain same number of cards as original deck")
+}
 
-  // shuffle
-  shuffledDeck, _ := Shuffle(newDeck)
+func TestShuffleValidateRandom(t *testing.T) {
+	// init new deck and shuffle
+	originalDeck, shuffledDeck := createNewDeckAndShuffle()
 
-  // validate same number of cards returned
-  assert.Equal(t, len(originalDeck), len(shuffledDeck), "Shuffled deck does not contain same number of cards as original deck")
+	// validate first ten cards in shuffled deck not same as first ten cards in original
+	numOfSameCards := 0
+	for idx := 0; idx < 10; idx++ {
+		if (originalDeck[idx].Suite == shuffledDeck[idx].Suite) &&
+			(originalDeck[idx].Value == shuffledDeck[idx].Value) {
+			numOfSameCards++
+		}
+	}
+	assert.NotEqual(t, numOfSameCards, 10, "First ten cards in shuffled deck same as original deck")
+}
 
-  // validate first ten cards in shuffled deck not same as first ten cards in original
-  numOfSameCards := 0
-  for idx := 0; idx < 10; idx++ {
-    if (originalDeck[idx].Suite == shuffledDeck[idx].Suite) &&
-      (originalDeck[idx].Value == shuffledDeck[idx].Value) {
-      numOfSameCards++
-    }
-  }
-  assert.NotEqual(t, numOfSameCards, 10, "First ten cards in shuffled deck same as original deck")
+func TestShuffleValidateNoDuplicate(t *testing.T) {
+	// init new deck and shuffle
+	_, shuffledDeck := createNewDeckAndShuffle()
 
-  // validate there are no duplicates after shuffle
-  existMap := make(map[string]int)
-  for _, card := range shuffledDeck {
-    if _, ok := existMap[card.String()]; ok {
-      // found a duplicate - fail the test immediately and break out of loop
-      assert.Fail(t, "Duplicate found: ", card.String())
-      break
-    } else {
-      // no duplicate - just increment counter in existMap
-      existMap[card.String()]++
-    }
-  }
+	// validate there are no duplicates after shuffle
+	existMap := make(map[string]int)
+	for _, card := range shuffledDeck {
+		if _, ok := existMap[card.String()]; ok {
+			// found a duplicate - fail the test immediately and break out of loop
+			assert.Fail(t, "Duplicate found: ", card.String())
+			break
+		} else {
+			// no duplicate - just increment counter in existMap
+			existMap[card.String()]++
+		}
+	}
 }
 
 func TestShuffleNilDeck(t *testing.T) {
-  // shuffle with no deck
-  _, error := Shuffle(nil)
-  assert.EqualError(t, error, "deck.Card is missing or empty")
+	// shuffle with no deck
+	_, error := Shuffle(nil)
+	assert.EqualError(t, error, "Card is missing or empty")
 }
 
 func TestShuffleEmptyDeck(t *testing.T) {
-  // shuffle with empty deck
-  var emptyDeck []Card
-  _, error := Shuffle(emptyDeck)
-  assert.EqualError(t, error, "deck.Card is missing or empty")
+	// shuffle with empty deck
+	var emptyDeck []Card
+	_, error := Shuffle(emptyDeck)
+	assert.EqualError(t, error, "Card is missing or empty")
+}
+
+func createNewDeckAndShuffle() ([]Card, []Card) {
+	// init new deck
+	newDeck := New()
+
+	// make copy of new deck since shuffle will change newDeck
+	var originalDeck = make([]Card, 52)
+	copy(originalDeck, newDeck)
+
+	// shuffle
+	shuffledDeck, _ := Shuffle(newDeck)
+
+	return originalDeck, shuffledDeck
 }
